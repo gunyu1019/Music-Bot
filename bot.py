68fa1fe92f969b4b0f271227465cd31220df1051
gunyu1019 <gunyu1019@gmail.com>
2020-08-25 20:54:12 +0900

    토큰관련으로 인한 깃 불가. 그러나 이젠 DB에 토큰값을 저장하기 때문에 상관없음.

--- bot.py
+++ bot.py
@@ -1,8 +1,39 @@
 import discord
 import asyncio
+import aiohttp
+
+import os
+import json
+import youtube_dl
+import pymysql
+
+from bs4 import BeautifulSoup
+
+directory = os.path.dirname(os.path.abspath(__file__)).replace("\\","/")
+db_f = open(directory + "/Data/bot_info.json",mode='r')
+db = db_f.read()
+db_f.close()
+db_json = json.loads(db)
+
+db_ip = db_json["mysql"]["ip"]
+db_user = db_json["mysql"]["user"]
+db_pw = db_json["mysql"]["password"]
+db_name = db_json["mysql"]["database"]
+
+
+connect = pymysql.connect(host=db_ip, user=db_user, password=db_pw,db=db_name, charset='utf8') #클라이언트 API키 불러오기.
+cur = connect.cursor()
+cur.execute("SELECT * from Music_Bot")
+client_list = cur.fetchone()
+key = client_list[0]
+token = client_list[1]
+connect.close()
 
 client = discord.Client()
 voice_channels = {}
+ydl_opts  = {
+    'format': 'bestaudio/bestaudio'
+}
 
 def get_voice(message):
     count = len(client.voice_clients)
@@ -11,6 +42,9 @@ def get_voice(message):
         if guildID == message.guild.id:
             return client.voice_clients[i]
     return None
+
+async def music(message,client):
+    return
 
 @client.event
 async def on_ready(): 
@@ -31,6 +65,8 @@ async def on_ready():
  
 @client.event
 async def on_message(message):
+    author_id = message.author.mention.replace("<@","",).replace(">","").replace("!","")
+    list_message = message.content.split(' ')
     prefix = '$'
     if message.content == f'{prefix}도움' or message.content == f'{prefix}도움말' or message.content == f'{prefix}help' or message.content == f'{prefix}명령어':
         embed = discord.Embed(color=0x0080ff)
@@ -73,6 +109,35 @@ async def on_message(message):
         embed = discord.Embed(title="MusicBot!",description=f"{voiceC.channel}에 성공적으로 떠났습니다!", color=0x0080ff)
         await message.channel.send(embed=embed)
         return
-
-with open('token.txt','r') as f:
-    client.run(f.read())
+    if message.content.startswith(f'{prefix}play'):
+        voiceC = get_voice(message)
+        if voiceC == None or not voiceC in voice_channels:
+            embed = discord.Embed(title="MusicBot!",description=f"음성채널방에 들어가있지 않습니다.", color=0xaa0000)
+            await message.channel.send(embed=embed)
+            return
+        if len(list_message) < 2:
+            embed = discord.Embed(title="MusicBot!",description=f"URL 혹은 영상 링크를 넣어주세요.", color=0xaa0000)
+            await message.channel.send(embed=embed)
+            return
+        music = " ".join(list_message[1:])
+        params = {
+            "part":"snippet",
+            "type":"vidoe",
+            "maxResults":1,
+            "key":key,
+            "q":music
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://www.googleapis.com/youtube/v3/search",params=params) as resp:
+                html = await resp.json()
+        if len(html['items']) == 0:
+            embed = discord.Embed(title="MusicBot!",description=f"검색결과가 없습니다..", color=0xaa0000)
+            await message.channel.send(embed=embed)
+            return
+        video_id = html['items'][0]['id']['videoId']
+        title = html['items'][0]['id']['snippet']['title']
+        cache = ydl_opts
+        cache['outtmpl'] = f'{directory}/{video_id}.mp3'
+        link = f'https://www.youtube.com/watch?v={video_id}'
+                
+client.run(token)
