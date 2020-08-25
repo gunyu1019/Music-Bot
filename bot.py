32e82f6c9c77cbe36c8d20c39dab147e2bbae6f1
gunyu1019 <gunyu1019@gmail.com>
2020-08-25 23:40:34 +0900

    재생목록 소화!

--- bot.py
+++ bot.py
@@ -21,7 +21,6 @@ db_user = db_json["mysql"]["user"]
 db_pw = db_json["mysql"]["password"]
 db_name = db_json["mysql"]["database"]
 
-
 connect = pymysql.connect(host=db_ip, user=db_user, password=db_pw,db=db_name, charset='utf8') #클라이언트 API키 불러오기.
 cur = connect.cursor()
 cur.execute("SELECT * from Music_Bot")
@@ -45,10 +44,46 @@ def get_voice(message):
     return None
 
 def download(video_id,link):
-    ydl_opts_cache = ydl_opts
-    ydl_opts_cache['outtmpl'] = f'{directory}/{video_id}.mp3'
-    with youtube_dl.YoutubeDL(ydl_opts_cache) as ydl:
-        ydl.download([link])
+    if not os.path.isfile(f'{directory}/{video_id}.mp3'):
+        ydl_opts_cache = ydl_opts
+        ydl_opts_cache['outtmpl'] = f'{directory}/Music_cache/{video_id}.mp3'
+        with youtube_dl.YoutubeDL(ydl_opts_cache) as ydl:
+            ydl.download([link])
+
+def f_thumbnail(video):
+    try:
+        thumbnail = video['snippet']['thumbnails']['default']['url']
+    except KeyError:
+        try:
+            thumbnail = video['snippet']['thumbnails']['high']['url']
+        except KeyError:
+            thumbnail = video['snippet']['thumbnails']['medium']['url']
+    finally:
+        return thumbnail
+
+async def playlist(voiceC,playlistId,author):
+    params = {
+        "part":"snippet",
+        "key":key,
+        "playlistId":playlistId
+    }
+    def append_channel(html,author):
+        for i in html['items']:
+            video_id = i['snippet']['resourceId']['videoId']
+            title = i['snippet']['title']
+            thumbnail = f_thumbnail(i)
+            download(video_id,f'https://www.youtube.com/watch?v={video_id}')
+            voice_channels[voiceC].append((video_id,title,author,thumbnail))
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://www.googleapis.com/youtube/v3/playlistItems",params=params) as resp:
+            html = await resp.json()
+    append_channel(html,author)
+    while "nextPageToken" in html:
+        params['pageToken'] = html['nextPageToken']
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://www.googleapis.com/youtube/v3/playlistItems",params=params) as resp:
+                html = await resp.json()
+        append_channel(html,author)
 
 @client.event
 async def on_ready(): 
@@ -125,37 +160,36 @@ async def on_message(message):
             return
         music = " ".join(list_message[1:])
         url_T = parse.urlparse(music)
-        if not (url_T.netloc.endswith('youtube.com')):
+        playlist_bool = False
+        if url_T.netloc.endswith('youtube.com'):
             for i in url_T.query.split('&'):
                 if i.startswith('list='):
+                    embed = discord.Embed(title="MusicBot!",description=f"재생목록을 다운로드받고 있습니다.\n상황에 따라 시간이 길게 소요될 수 있으니, 양해부탁드립니다.", color=0x0080ff)
+                    await message.channel.send(embed=embed)
                     music = music.replace(i,'')
-        params = {
-            "part":"snippet",
-            "type":"vidoe",
-            "maxResults":1,
-            "key":key,
-            "q":music
-         }
-        async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://www.googleapis.com/youtube/v3/search",params=params) as resp:
-                html = await resp.json()
-        if len(html['items']) == 0:
-            embed = discord.Embed(title="MusicBot!",description=f"검색결과가 없습니다..", color=0xaa0000)
-            await message.channel.send(embed=embed)
-            return
-        video = html['items'][0]
-        video_id = video['id']['videoId']
-        title = video['snippet']['title']
-        try:
-            thumbnail = video['snippet']['thumbnails']['default']['url']
-        except KeyError:
-            try:
-                thumbnail = video['snippet']['thumbnails']['high']['url']
-            except KeyError:
-                thumbnail = video['snippet']['thumbnails']['medium']['url']
-        if not os.path.isfile(f'{directory}/{video_id}.mp3'):
+                    await playlist(voiceC,i.split('list=')[1],message.author)
+                    playlist_bool = True
+        if not playlist_bool:
+            params = {
+                "part":"snippet",
+                "type":"vidoe",
+                "maxResults":1,
+                "key":key,
+                "q":music
+            }
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f"https://www.googleapis.com/youtube/v3/search",params=params) as resp:
+                    html = await resp.json()
+            if len(html['items']) == 0:
+                embed = discord.Embed(title="MusicBot!",description=f"검색결과가 없습니다..", color=0xaa0000)
+                await message.channel.send(embed=embed)
+                return
+            video = html['items'][0]
+            video_id = video['id']['videoId']
+            title = video['snippet']['title']
+            thumbnail = f_thumbnail(video)
+            author = message.author
             download(video_id,f'https://www.youtube.com/watch?v={video_id}')
+            voice_channels[voiceC].append((video_id,title,author,thumbnail))
         
-        
-                
 client.run(token)
